3f37d52ee8aa11a102f4e0d0e9d7d0450d870533
SaeedoSunbul <saeedsunbul@outlook.com>
2024-08-12 01:46:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
@@ -7,13 +7,13 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     var buttonInnerHTML = this.innerHTML; // the butoon w ,a,s,d,j,k,l 
     console.log(buttonInnerHTML)
 
- /*   if ( buttonInnerHTML === "w"){
-      var audio1 = new Audio("https://www.vibrationdata.com/tuningfork440.mp3");
+   if ( buttonInnerHTML === "w"){
+      var audio1 = new Audio("Anime wow sound effect [TubeRipper.com].mp3");
       audio1.play() ;
       audio1.volume = 0.5;
     } 
     if ( buttonInnerHTML === "a"){
-      var audio2 = new Audio("https://www.vibrationdata.com/tuningfork440.mp3");
+      var audio2 = new Audio("Cartoon Bonk Sound Effect [TubeRipper.com].mp3");
       audio2.play() ;
       audio2.volume = 0.5;
     }  
@@ -23,22 +23,22 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
       audio3.volume = 0.5;
     } 
     if ( buttonInnerHTML === "d"){
-      var audio4 = new Audio("https://www.vibrationdata.com/piano_C_sharp.mp3");
+      var audio4 = new Audio("Slap oh Sound effect [TubeRipper.com].mp3");
       audio4.play() ;
       audio4.volume = 0.5;
     }  
     if ( buttonInnerHTML === "j"){
-      var audio5 = new Audio("https://www.vibrationdata.com/piano_D_sharp.mp3");
+      var audio5 = new Audio("رياكشن وفرها في جيبك يالعبد [TubeRipper.com].mp3");
       audio5.play();
       audio5.volume = 0.5;
     }           
     if ( buttonInnerHTML === "k"){
-      var audio6 = new Audio("https://www.vibrationdata.com/tuningfork440.mp3");
+      var audio6 = new Audio("sounds\tom-3.mp3");
       audio6.play(); 
       audio6.volume = 0.5;
     }
     if ( buttonInnerHTML === "l"){
-      var audio7 = new Audio("https://www.vibrationdata.com/tuningfork440.mp3");
+      var audio7 = new Audio("sounds\tom-4.mp3");
       audio7.play(); 
       audio7.volume = 0.5;
     }
@@ -47,7 +47,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
   });
 }
-*
+/*
 document.addEventListener("keypress", function() {
   
   var buttonInnerHTML = this.innerHTML
